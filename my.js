75e653c51bfd9af8fd53ce467aca9edf58092fe0
Prathvinagar <123464619+Prathvinagar@users.noreply.github.com>
2023-02-28 18:30:09 +0530

    Add files via upload
    test

---
+++ my.js
@@ -0,0 +1,63 @@
+// object question 1
+
+
+// const book = {
+//     title:"Oneman army",
+//     author:"J.k",
+//     year:1995,
+//     publisher:"Sdm publisher"
+// }
+// console.log(Object.keys(book))
+
+// object question 2
+
+
+// const book = {
+//     title:"Oneman army",
+//     author:"J.k",
+//     year:1995,
+//     publisher:"Sdm publisher"
+// }
+// console.log(Object.values(book));
+
+
+//  object question 2
+
+
+// const book = {
+//     title:"Oneman army",
+//     author:"J.k",
+//     year:1995,
+//     publisher:"Sdm publisher"
+// }
+// console.log(Object.entries(book));
+
+// object question 2
+
+// const student={
+//     'name':'Vinod',
+//     'city':'Bhopal'
+// }
+
+
+// function checkProperty(obj,propertyName){
+//         return obj.hasOwnProperty(propertyName)
+// }
+
+// console.log(checkProperty(student,'name'));
+
+// object question 4
+
+// const book = {
+//     title:"Oneman army",
+//     author:"J.k",
+//     year:1995,
+//     publisher:"Sdm publisher"
+// }
+
+// function convert (obj) {
+//     return JSON.stringify(obj)
+// }
+
+// console.log(convert(book));
+
